734b0490dc6ef441ac5e1bfb8e5f41488d15c72d
ddolpali <matt@aegyo.pro>
2017-04-10 11:44:11 -0700

    add header banner and route entries

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,15 +11,27 @@
 
 <body>
     <header>
-        <nav class="z-depth-0">
-            <div class="nav-wrapper">
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="{{ route('portfolio') }}">Portfolio</a></li>
-                    <li><a href="{{ route('tos') }}">TOS</a></li>
-                    <li><a href="{{ route('contact') }}">Contact</a></li>
-                </ul>
+        <div class="banner">
+            <nav class="z-depth-0">
+                <div class="nav-wrapper">
+                    <ul id="nav" class="right hide-on-med-and-down">
+                        <li><a class="waves-effect waves-light btn">Hire Me</a></li>
+                    </ul>
+                    <ul id="nav" class="right text-nav">
+                        <li><a href="{{ route('portfolio') }}">Portfolio</a></li>
+                        <li><a href="{{ route('tos') }}">TOS</a></li>
+                        <li><a href="{{ route('contact') }}">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="banner-text">
+                <h1>mungjar</h1>
             </div>
-        </nav>
+
+            <div class="container"></div>
+        </div>
+
     </header>
     <main>
 
@@ -30,6 +42,11 @@
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="{{ asset('js/materialize.js') }}"></script>
+    <script>
+        $( document ).ready(function(){
+            $(".button-collapse").sideNav();
+        });
+    </script>
 </body>
 
 </html>
